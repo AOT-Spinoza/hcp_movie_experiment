2af07e3a5cde4b625ab858a04795b6f6c5f2ec3f
shufan <shufan.z.t@gmail.com>
2024-04-24 14:29:33 +0200

    test

--- experiment/main.py
+++ experiment/main.py
@@ -12,7 +12,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('subject', default=1, nargs='?')
 parser.add_argument('run', default=1, nargs='?')
 parser.add_argument('eyelink', default=False, nargs='?')
-
+#
 cmd_args = parser.parse_args()
 subject, run, eyelink = cmd_args.subject, cmd_args.run, cmd_args.eyelink
 
@@ -38,11 +38,12 @@ output_str = f'sub-{subject}_run-{run}_task-movie'
 settings_fn = op.join(op.dirname(__file__), 'settings.yml')
 
 session_object = HCPMovieELSession(output_str=output_str,
-                        output_dir=None,
-                        settings_file=settings_fn, 
-                        eyetracker_on=eyetracker_on,
-                        which_movie=run)
+                                   output_dir=None,
+                                   settings_file=settings_fn,
+                                   eyetracker_on=eyetracker_on,
+                                   which_movie=run)
 session_object.create_trials()
-logging.warn(f'Writing results to: {op.join(session_object.output_dir, session_object.output_str)}')
+logging.warn(
+    f'Writing results to: {op.join(session_object.output_dir, session_object.output_str)}')
 session_object.run()
 session_object.close()
